3df6dee408fe8fa891ccdd7771b68cf7a9844554
Naitik <naitikraj12935@gmail.com>
2023-11-14 13:18:46 +0530

    remove client

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const cors = require('cors');
 const followRoute = require('./Route/followroute');
 dotenv.config();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+
 
 app.use(express.json());
 app.use(express.static('public'));
